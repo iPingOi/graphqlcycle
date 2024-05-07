c84a2f5979c586e99e48d7c8399e37574ef3cc11
Gabriel C. Almeida <gabriel.alvikar@gmail.com>
2024-05-07 04:30:11 -0300

    feat: create find all for categories

--- internal/database/category.go
+++ internal/database/category.go
@@ -2,13 +2,14 @@ package database
 
 import (
 	"database/sql"
+
 	"github.com/google/uuid"
 )
 
 type Category struct {
-	db *sql.DB
-	ID string
-	Name string
+	db          *sql.DB
+	ID          string
+	Name        string
 	Description string
 }
 
@@ -25,4 +26,21 @@ func (c *Category) Create(name string, description string) (Category, error) {
 	}
 
 	return Category{ID: id, Name: name, Description: description}, nil
+}
+
+func (c *Category) FindAll() ([]Category, error) {
+	rows, err := c.db.Query("SELECT id, name, description FROM categories")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	categories := []Category{}
+	for rows.Next() {
+		var id, name, description string
+		if err := rows.Scan(&id, &name, &description); err != nil {
+			return nil, err
+		}
+		categories = append(categories, Category{ID: id, Name: name, Description: description})
+	}
+	return categories, nil
 }
